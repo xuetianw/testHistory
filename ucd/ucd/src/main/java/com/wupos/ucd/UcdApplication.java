dbbe6029c584558c1fcfc7be527d81ff534ae70b
Jacob Seol <jacob.seol@hcl.com>
2023-08-16 10:48:56 -0700

    add Spring project

---
+++ ucd/ucd/src/main/java/com/wupos/ucd/UcdApplication.java
@@ -0,0 +1,13 @@
+package com.wupos.ucd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UcdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UcdApplication.class, args);
+	}
+
+}
---
+++ ucd/ucd/src/test/java/com/wupos/ucd/UcdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wupos.ucd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UcdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
